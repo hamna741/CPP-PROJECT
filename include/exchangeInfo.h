26a29ff04c74cd713beb3053675d928d2cda47ae
hamna noor <hamna@rakurai.io>
2023-05-29 18:39:48 +0500

    added required map varaible

--- include/exchangeInfo.h
+++ include/exchangeInfo.h
@@ -1,5 +1,7 @@
+/*
 #include <iostream>
 #include <string>
+#include <regex>
 #include <cpprest/http_client.h>
 #include <cpprest/filestream.h>
 #include <rapidjson/document.h>
@@ -7,34 +9,93 @@
 #include <rapidjson/stringbuffer.h>
 #include <rapidjson/istreamwrapper.h>
 #include <rapidjson/prettywriter.h>
-#include <rapidjson/ostreamwrapper.h> 
+#include <rapidjson/ostreamwrapper.h>
+#include <thread>
+#include <chrono>
+#include <time.h>
+#include "spdlog/spdlog.h"
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include "spdlog/sinks/basic_file_sink.h"
+
+namespace exchangeInfo
+{
+    class exchangeInfoClass
+    {
+    private:
+        std::string jsonData;
+        int id;
+        std::vector<int> prevID;
+        std::string instrumentName;
+        std::vector<std::string> prevInstrumentName;
+        std::map<int, std::string> prevData;
+        std::string contractType;
+        std::string tickSize;
+        std::string stepSize;
+        std::string filterType;
+        std::string status;
+        bool fileLog, consoleLog;
+        std::shared_ptr<spdlog::logger> fileLogger;
+        std::shared_ptr<spdlog::logger> consoleLogger;
+
+    public:
+        exchangeInfoClass();
+        int readQueryFile();
+        void getExchangeInfo();
+        void deleteData(int id, std::string instrumentName);
+        void getData(std::string instrumentName);
+        void updatetData(const rapidjson::Value &queryObject);
+        void queryCheck(std::string &queryContent);
+        void configFunc();
+        void setlogger();
+        ~exchangeInfoClass();
+    };
+}
 
 
-namespace exchangeInfo{
-class exchangeInfoClass{
+*/
+#ifndef EXCHANGE_INFO_H
+#define EXCHANGE_INFO_H
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <regex>
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/istreamwrapper.h>
+#include <rapidjson/prettywriter.h>
+#include <rapidjson/ostreamwrapper.h>
+#include <thread>
+#include <chrono>
+#include <time.h>
+#include "spdlog/spdlog.h"
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include "spdlog/sinks/basic_file_sink.h"
+
+class ExchangeInfoClass
+{
 private:
-    std::string jsonData;
+    std::map<std::string, rapidjson::Value> symbolDataMap;
     int id;
-    std::vector<int> prevID;
-    std::string instrumentName ;
-    std::vector<std::string> prevInstrumentName;
-    std::string contractType;
-    std::string tickSize;
-    std::string stepSize;
-    std::string filterType;
-    std::string status;
-
-
+    std::string instrumentName;
+    bool fileLog, consoleLog;
+    std::map<int, std::string> prevData;
+    std::shared_ptr<spdlog::logger> fileLogger;
+    std::shared_ptr<spdlog::logger> consoleLogger;
 
 public:
-    exchangeInfoClass();
-    int readQueryFile();
+    //  ExchangeInfoClass();
+    void configFunc();
     void getExchangeInfo();
-    void deleteData();
-    void getData(std::string instrumentName);
-    void updatetData();
-    ~exchangeInfoClass();
-
-
+    void readQueryFile();
+    void queryCheck(std::string &queryContent);
+    void deleteData(int id, std::string instrumentName);
+    void getData(const std::string& instrumentName);
+    void updatetData(const rapidjson::Value &queryObject);
+   
 };
-}
+
+#endif // EXCHANGE_INFO_H
