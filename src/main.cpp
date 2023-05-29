b761253ce3bdc9398948428a3e0573f11455db0c
hamna noor <hamna@rakurai.io>
2023-05-29 21:55:01 +0500

    added multithreading to get exchange fucntion

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,19 @@ int main()
   /////////////////////////////////////////////////
   
   ExchangeInfoClass info;
-  info.configFunc();
-info.getExchangeInfo();
+  int interval=info.configFunc();
+  //info.getExchangeInfo();
+   std::thread exchangeInfoThread([&]() {
+        while (true) {
+            info.getExchangeInfo();
+            std::this_thread::sleep_for(std::chrono::seconds(interval));
+        }
+    });
+
+
    info.readQueryFile();
    //  info.getExchangeInfo();
-   std::cout<<"dfs";
+
    // info.readQueryFile();
    // info.getData();
    
