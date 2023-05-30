c03fd094dbeeb22be6c77f6e6897994f38d73894
hamna noor <hamna@rakurai.io>
2023-05-30 17:49:47 +0500

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,13 @@ int main()
   
   ExchangeInfoClass info;
   int interval=info.configFunc();
-  //info.getExchangeInfo();
-   std::thread exchangeInfoThread([&]() {
-        while (true) {
-            info.getExchangeInfo();
-            std::this_thread::sleep_for(std::chrono::seconds(interval));
-        }
-    });
+  info.getExchangeInfo();
+  //  std::thread exchangeInfoThread([&]() {
+  //       while (true) {
+  //           info.getExchangeInfo();
+  //           std::this_thread::sleep_for(std::chrono::seconds(interval));
+  //       }
+  //   });
 
 
    info.readQueryFile();
@@ -25,10 +25,6 @@ int main()
 
    // info.readQueryFile();
    // info.getData();
-   
-/////////////////////////////////////////////////////////////////////////////////////////RETRIEVING EXCHANGE INFO
-   
-///////////////////////////////////////////////////////////////////////////////////////////////////// READING QUERY FILE
 
     return 0;
 }
