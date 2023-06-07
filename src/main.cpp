17b85a5347d984ecc8ab0a9d0f3b982284447303
hamna noor <hamna@rakurai.io>
2023-06-07 21:30:51 +0500

    fixed polling of getExchnageInfo function

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,47 @@
 #include <iostream>
 #include "exchangeInfo.h"
 
-
+ExchangeInfoClass info;
+void reloadData(int interval)
+{
+    while (true)
+    {
+        info.getBoostStruct();
+        std::this_thread::sleep_for(std::chrono::seconds(interval));
+    }
+}
+void readQuery()
+{
+   
+        info.readQueryFile();
+       
+}
 int main()
-{  
- 
- //info.extractSymbols(jsonString);
-
-  /////////////////////////////////////////////////
-  
-  ExchangeInfoClass info;
-  int interval=info.configFunc();
-  info.getExchangeInfo();
-  //  std::thread exchangeInfoThread([&]() {
-  //       while (true) {
-  //           info.getExchangeInfo();
-  //           std::this_thread::sleep_for(std::chrono::seconds(interval));
-  //       }
-  //   });
+{
 
 
-   info.readQueryFile();
-   //  info.getExchangeInfo();
+    int interval = info.configFunc();
+    //  while (true)
+    // {
+    //     info.getBoostStruct();
+    //     std::this_thread::sleep_for(std::chrono::seconds(interval));
+    // }
+      std::thread reloadThread(reloadData,interval);
+    //  helper.detach();
+   // helper.join();
+    // info.getBoostStruct();
+//  auto exec_run = [](){ while (true) info.getBoostStruct(); };
+//     std::thread t(exec_run);
+//     t.join();
+sleep(interval+5);
+// std::cout<<"waited";
+std::thread queryThread(readQuery);
+       queryThread.join();
+    // info.getExchangeInfo();
 
-   // info.readQueryFile();
-   // info.getData();
+  //   info.readQueryFile();
+    //  info.getData();
+    // info.foo();
 
     return 0;
 }
