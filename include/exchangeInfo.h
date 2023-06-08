ae3959959b4b81e32c461ac04af8e882b54e8e05
hamna noor <hamna@rakurai.io>
2023-06-08 22:08:11 +0500

    added get ans set functions

--- include/exchangeInfo.h
+++ include/exchangeInfo.h
@@ -1,67 +1,13 @@
-/*
-#include <iostream>
-#include <string>
-#include <regex>
-#include <cpprest/http_client.h>
-#include <cpprest/filestream.h>
-#include <rapidjson/document.h>
-#include <rapidjson/writer.h>
-#include <rapidjson/stringbuffer.h>
-#include <rapidjson/istreamwrapper.h>
-#include <rapidjson/prettywriter.h>
-#include <rapidjson/ostreamwrapper.h>
-#include <thread>
-#include <chrono>
-#include <time.h>
-#include "spdlog/spdlog.h"
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include "spdlog/sinks/basic_file_sink.h"
 
-namespace exchangeInfo
-{
-    class exchangeInfoClass
-    {
-    private:
-        std::string jsonData;
-        int id;
-        std::vector<int> prevID;
-        std::string instrumentName;
-        std::vector<std::string> prevInstrumentName;
-        std::map<int, std::string> prevData;
-        std::string contractType;
-        std::string tickSize;
-        std::string stepSize;
-        std::string filterType;
-        std::string status;
-        bool fileLog, consoleLog;
-        std::shared_ptr<spdlog::logger> fileLogger;
-        std::shared_ptr<spdlog::logger> consoleLogger;
-
-    public:
-        exchangeInfoClass();
-        int readQueryFile();
-        void getExchangeInfo();
-        void deleteData(int id, std::string instrumentName);
-        void getData(std::string instrumentName);
-        void updatetData(const rapidjson::Value &queryObject);
-        void queryCheck(std::string &queryContent);
-        void configFunc();
-        void setlogger();
-        ~exchangeInfoClass();
-    };
-}
-
-
-*/
 #ifndef EXCHANGE_INFO_H
 #define EXCHANGE_INFO_H
 
 #include <vector>
 #include <iostream>
 #include <string>
-#include <regex>
 #include <cpprest/http_client.h>
-#include <cpprest/filestream.h>
+// #include <cpprest/filestream.h>
+#include<fstream>
 #include <rapidjson/document.h>
 #include <rapidjson/writer.h>
 #include <rapidjson/stringbuffer.h>
@@ -70,32 +16,28 @@ namespace exchangeInfo
 #include <rapidjson/ostreamwrapper.h>
 #include <thread>
 #include <chrono>
-#include <time.h>
 #include "spdlog/spdlog.h"
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include "spdlog/sinks/basic_file_sink.h"
 #include <boost/asio.hpp>
-#include <boost/property_tree/json_parser.hpp>
-#include <boost/property_tree/ptree.hpp>
-// #include <boost/beast/core.hpp>
-// #include <boost/beast/http.hpp>
-// #include <boost/beast/version.hpp>
-// namespace beast = boost::beast;         // from <boost/beast.hpp>
-// namespace http = beast::http; 
+// #include <boost/asio/ssl.hpp>
+
 using boost::asio::ip::tcp;
 class ExchangeInfoClass
 {
 private:
-  //  std::map<std::string, rapidjson::Value> symbolDataMap;
-    struct SymbolData {
-    std::string symbolName;
-    std::string status;
-    std::string contractType;
-    std::string tickSize;
-    std::string stepSize;
-};
-    int id;
-     std::vector<SymbolData> symbolDataVec;
+   
+    struct SymbolData
+    {
+        std::string symbolName;
+        std::string status;
+        std::string contractType;
+        std::string tickSize;
+        std::string stepSize;
+    };
+   
+   SymbolData dataObject;
+    std::vector<SymbolData> symbolDataVec;
     std::string instrumentName;
     bool fileLog, consoleLog;
     std::string url;
@@ -104,7 +46,18 @@ private:
     std::shared_ptr<spdlog::logger> consoleLogger;
 
 public:
-    //  ExchangeInfoClass();
+    void setSymbolName(std::string symName);
+    void setstatus(std::string stat);
+    void setContractType(std::string CType);
+    void setTicksize(std::string TSize);
+    void setStepSize(std::string SSize);
+
+    std::string getSymbolName();
+    std::string getContractType();
+    std::string getStatus();
+    std::string getTickSize();
+    std::string getStepSize();
+  
     int configFunc();
     // void getExchangeInfo();
     void readQueryFile();
@@ -114,7 +67,7 @@ public:
     void updatetData(const rapidjson::Value &queryObject);
     // void getBoostExchangeInfo();
     void getBoostStruct();
-    void getDataStruct(const std::string& instrumentName, const rapidjson::Value& queryContent);
+    void getDataStruct(const std::string &instrumentName, const rapidjson::Value &queryContent);
 
 };
 
